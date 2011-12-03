8489c2791e10dcfd6baeb40e56b2e1ccf23ea379
Wojciech Rakszawski <wojciech.raq@gmail.com>
2011-12-03 20:48:05 +0100

    First scratch off Mesh class completed

---
+++ src/Mesh.cpp
@@ -0,0 +1,41 @@
+
+#include "Mesh.h"
+
+Mesh::Mesh(std::string name, const Mesh::meshData* data, Material* material) :
+Resource(name),
+vertexPosition(data->vertexPosition),
+textureCoords(data->textureCoords),
+vertexNormals(data->vertexNormals),
+vertexIndexes(data->vertexPosition),
+material(material)
+{
+}
+
+Mesh::~Mesh()
+{
+}
+
+void Mesh::Render()
+{
+  //setting up material
+  this->material->Render();
+
+  //enable object properietes
+  glEnableClientState(GL_NORMAL_ARRAY);
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+  //filling data
+  glNormalPointer(GL_FLOAT, 0, &(this->vertexNormals[0]));
+  glVertexPointer(3, GL_FLOAT, 0, &(this->vertexPosition[0]));
+  glTexCoordPointer(2, GL_FLOAT, 0, &(this->textureCoords[0]));
+
+  //rendering
+  //glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, this->vertexIndexes.size(), GL_UNSIGNED_SHORT,
+      &(this->vertexIndexes[0]));
+
+  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+  glDisableClientState(GL_VERTEX_ARRAY); // disable vertex arrays
+  glDisableClientState(GL_NORMAL_ARRAY);
+}
--- src/Mesh.h
+++ src/Mesh.h
@@ -1,14 +1,33 @@
 #include "Resource.h"
+#include "Material.h"
 #include <vector>
+#include <GL/gl.h>
 
 #ifndef MESH_H
 #define MESH_H
 
 class Mesh: public Resource
 {
+  struct meshData
+  {
+    std::vector<GLfloat> vertexPosition;
+    std::vector<GLfloat> textureCoords;
+    std::vector<GLfloat> vertexNormals;
+    std::vector<GLfloat> vertexIndexes;
+  };
+  
 public:  
-  Mesh(std::string name, std::vector<float> data, std::vector<int> indexes);
+  Mesh(std::string name, const Mesh::meshData* data, Material* material);
   virtual ~Mesh();
+  
+  virtual void Render();
+  
+private:
+    std::vector<GLfloat> vertexPosition;
+    std::vector<GLfloat> textureCoords;
+    std::vector<GLfloat> vertexNormals;
+    std::vector<GLfloat> vertexIndexes;
+    Material* material;
 };
 
 #endif 
